92a1ad6d160a7d25598d932ee5500ecd4f60c2c1
Pedro Revez <pedroallenrevez@gmail.com>
2020-03-14 20:02:36 +0000

    Backend now has logging.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,23 @@
+import logging
+logging.basicConfig(
+    level=2,
+    format="[%(asctime)s][SELF] [%(levelname)s] %(message)s"
+)
+
 from fastapi import FastAPI
 import firebase_admin as fb
 from firebase_admin import credentials
 
+try:
+    cred = credentials.Certificate("secrets.json")
+    fb.initialize_app(cred)
+    logging.info("Successfully connected to Firebase as Admin.")
+except Exception as e:
+    logging.warning("Could not connect to Firebase as admin. Malformed credentials? Secrets must be in the same folder as app.")
+
+
 app = FastAPI()
-
-cred = credentials.Certificate("secrets.json")
-fb.initialize_app(cred)
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
